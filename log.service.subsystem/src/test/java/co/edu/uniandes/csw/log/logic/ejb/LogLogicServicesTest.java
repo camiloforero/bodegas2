444a044946bc90b157ec28250c4e84012cfdb74e
AndresLeon1 <ad.leon2282@uniandes.edu.co>
2014-05-23 09:51:38 -0500

    Pruebas del componente Log (Clase, Package y pruebas añadidas)

---
+++ log.service.subsystem/src/test/java/co/edu/uniandes/csw/log/logic/ejb/LogLogicServicesTest.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.uniandes.csw.log.logic.ejb;
+
+import co.edu.uniandes.csw.bodega.persistence.entity.BodegaEntity;
+import co.edu.uniandes.csw.log.logic.api.ILogLogicService;
+import co.edu.uniandes.csw.log.logic.dto.LogDTO;
+import co.edu.uniandes.csw.log.persistence.LogPersistence;
+import co.edu.uniandes.csw.log.persistence.api.ILogPersistence;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import java.util.*;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author admin
+ */
+@RunWith(Arquillian.class)
+public class LogLogicServicesTest {
+    public static final String DEPLOY = "Prueba";
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, DEPLOY + ".war")
+				.addPackage(LogLogicService.class.getPackage())
+				.addPackage(LogPersistence.class.getPackage())
+				.addPackage(BodegaEntity.class.getPackage())
+				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource("META-INF/beans.xml", "beans.xml");
+	}
+        
+        @Inject
+	private ILogLogicService bodegaLogicService;
+	
+	@Inject
+	private ILogPersistence bodegaPersistence;
+        
+        @Before
+	public void configTest() {
+		try {
+			clearData();
+			insertData();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void clearData() {
+		List<LogDTO> dtos=bodegaPersistence.getLogs();
+		for(LogDTO dto:dtos){
+			bodegaPersistence.deleteLog(dto.getId());
+		}
+	}
+
+	private List<LogDTO> data=new ArrayList<LogDTO>();
+
+	private void insertData() {
+		for(int i=0;i<3;i++){
+			LogDTO pdto=new LogDTO();
+			pdto.setName(generateRandom(String.class));
+			pdto=bodegaPersistence.createLog(pdto);
+			data.add(pdto);
+		}
+	}
+        
+        @Test
+        public void createLogTest(){
+		LogDTO ldto=new LogDTO();
+		ldto.setName(generateRandom(String.class));
+		
+		
+		LogDTO result=bodegaLogicService.createLog(ldto);
+		
+		Assert.assertNotNull(result);
+		
+		LogDTO pdto=bodegaPersistence.getLog(result.getId());
+		
+		Assert.assertEquals(ldto.getName(), pdto.getName());	
+	}
+        
+        public <T> T generateRandom(Class<T> objectClass){
+		Random r=new Random();
+		if(objectClass.isInstance(String.class)){
+			String s="";
+			for(int i=0;i<10;i++){
+				char c=(char)(r.nextInt()/('Z'-'A')+'A');
+				s=s+c;
+			}
+			return objectClass.cast(s);
+		}else if(objectClass.isInstance(Integer.class)){
+			Integer s=r.nextInt();
+			return objectClass.cast(s);
+		}else if(objectClass.isInstance(Long.class)){
+			Long s=r.nextLong();
+			return objectClass.cast(s);
+		}else if(objectClass.isInstance(java.util.Date.class)){
+			java.util.Calendar c=java.util.Calendar.getInstance();
+			c.set(java.util.Calendar.MONTH, r.nextInt()/12);
+			c.set(java.util.Calendar.DAY_OF_MONTH,r.nextInt()/30);
+			c.setLenient(false);
+			return objectClass.cast(c.getTime());
+		} 
+		return null;
+	}
+}
