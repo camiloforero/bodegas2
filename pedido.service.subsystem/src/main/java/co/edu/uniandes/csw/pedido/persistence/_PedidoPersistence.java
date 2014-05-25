acf785af4e9dfabbea985fce162434fdb53209e4
EmilNamen <ec.namen1091@uniandes.edu.co>
2014-05-25 17:56:43 -0500

    Envia mensaje al agregar pedido

--- pedido.service.subsystem/src/main/java/co/edu/uniandes/csw/pedido/persistence/_PedidoPersistence.java
+++ pedido.service.subsystem/src/main/java/co/edu/uniandes/csw/pedido/persistence/_PedidoPersistence.java
@@ -11,6 +11,14 @@ import co.edu.uniandes.csw.pedido.logic.dto.PedidoDTO;
 import co.edu.uniandes.csw.pedido.persistence.api._IPedidoPersistence;
 import co.edu.uniandes.csw.pedido.persistence.converter.PedidoConverter;
 import co.edu.uniandes.csw.pedido.persistence.entity.PedidoEntity;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public abstract class _PedidoPersistence implements _IPedidoPersistence {
 
@@ -20,6 +28,7 @@ public abstract class _PedidoPersistence implements _IPedidoPersistence {
 	public PedidoDTO createPedido(PedidoDTO pedido) {
 		PedidoEntity entity=PedidoConverter.persistenceDTO2Entity(pedido);
 		entityManager.persist(entity);
+                 enviarCorreo(pedido.getId(), pedido.getName(), pedido.getCantidad());
 		return PedidoConverter.entity2PersistenceDTO(entity);
 	}
 
@@ -43,4 +52,42 @@ public abstract class _PedidoPersistence implements _IPedidoPersistence {
 		PedidoConverter.entity2PersistenceDTO(entity);
 	}
 
+        public void enviarCorreo(Long id, String nombrePedido, Integer cantidad ) 
+        {
+                Properties props = new Properties();
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.starttls.enable", "true");
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.port", "587");
+ 
+                Session session = Session.getInstance(props, new javax.mail.Authenticator() 
+                {
+                        protected PasswordAuthentication getPasswordAuthentication()
+                        {
+                                return new PasswordAuthentication("equiporocket2014@gmail.com", "E2014rocket");
+                        }
+                  });
+ 
+                try {
+ 
+                        Message message = new MimeMessage(session);
+                        message.setFrom(new InternetAddress("equiporocket2014@gmail.com"));
+                        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("equiporocket2014@gmail.com"));
+                        message.setSubject("Nuevo pedido agregado al sistema");
+                        message.setText("Se ha agregado el pedido con id "+id+",\n "
+                                + "con nombre "+nombrePedido+",\n "
+                                + "de cantidad "+cantidad+",\n "
+                                + "Gracias por su atencion.\n "
+                                + "Equipo Rocket 2014  ");
+ 
+                        Transport.send(message);
+ 
+                        System.out.println("Done");
+ 
+                } 
+                catch (MessagingException e) 
+                {
+                        throw new RuntimeException(e);
+                }
+        }
 }
